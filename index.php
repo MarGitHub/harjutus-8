034643d2a438b6061cce24c44959065942a0b5a2
Margit <margit.pilden@gmail.com>
2014-09-26 12:30:25 +0300

    ex 8

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE HTML!>
+<html>
+<head>
+	<title>harjutus - 8</title>
+	<meta charset="UTF-8">
+</head>
+<body>
+
+	<h2>Funktsioon "continue"</h2>
+	<?php
+for ($number = 1; $number <= 10; $number++) {
+    if ($number == 5) {
+        continue;
+      
+    }
+
+    echo $number . ", ";
+}
+?>
+<h2>Funktsioon "break"</h2>
+	<?php
+for ($number = 1; $number <= 10; $number++) {
+    if ($number == 6) {
+        break;
+
+    }
+
+    echo $number . ", ";
+}
+?>
+
+</body>
+</html>
